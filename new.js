5b91ca8ead1fddf29f8cd34100b788f5525a09b9
hisato-kozuki <167668521+hisato-kozuki@users.noreply.github.com>
2024-12-27 16:10:59 +0900

    Update new.js

--- new.js
+++ new.js
@@ -1,3 +1,7 @@
 import a from './aaa.js'
 console.log(a);
 document.write('<h1>HTML</h1>')
+const output        = document.currentScript;
+const outputTag     = document.createElement('h1');
+outputTag.innerHTML = 'HTML';
+output.parentNode.appendChild(outputTag);
